eb7a1b2f46ebedc6c57f8c97f6af52c3c3a4b22f
Serikova Tanya <serikova-03-91@mail.ru>
2020-06-15 17:43:47 +0600

    progres 15.06

--- js/common.js
+++ js/common.js
@@ -1,19 +1,24 @@
-$(document).ready(function(){
+jQuery(document).ready(function(){
 
     jQuery('.parent-slider').owlCarousel({
 		//items: 4,
 		dots: true,
 		nav: true,
+		loop: true,
+		mouseDrag: true,
+		touchDrag: true,
 		navText: Array('<img src="img/arrow-left.svg">',  '<img src="img/arrow-right.svg">'),
 		responsive: {
 
 			0: {
 				items: 1
+
 			},
 
 			768: {
 				items: 1
 			},
+
 
 			1024: {
 				items: 1
@@ -23,6 +28,51 @@ $(document).ready(function(){
 
 		}
 	});
+
+
+
+
+    // Попап
+	var click_popap = jQuery('.click-popap');
+
+	click_popap.on('click', function(){
+
+		var modal_content = jQuery(this).next();
+		modal_content.css({"display": "block"});
+		var modal_width = modal_content.width();
+
+		jQuery(this).next().addClass("modal-open-parent");
+
+		jQuery('.modal-content').addClass("modal-content-show");
+
+
+		// Закрыть попап
+		jQuery('.modal-open-parent').on('click', function(event){
+
+			if(event.target.className == 'modal-close modal-open-parent') {
+				modal_content.css({"display": "none"});
+				event.target.classList.remove('modal-open-parent');
+			}
+
+        });     
+
+	
+
+			
+
+
+
+		//Закрыть попап кнопкой
+		$('.close-button').on('click', function(){
+			modal_content.css({"display": "none"});
+			modal_content.removeClass('modal-content-show');
+			$('.modal-open-black').removeClass('modal-open-parent');
+		});
+			
+
+	});
+
+
 
 
 });
